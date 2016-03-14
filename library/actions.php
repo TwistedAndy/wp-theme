ad7d60c019827d07ab4058689b09b69ac9a0825c
TwistedAndy <toniyevych@gmail.com>
2016-03-14 19:08:57 +0200

    Added an ability to clear the canonical links in wp_head()

--- library/actions.php
+++ library/actions.php
@@ -45,15 +45,17 @@ if (!is_admin() and tw_settings('init', 'action_clean_header')) {
 	
 		remove_action('wp_head', 'wp_generator');
 		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'rel_canonical');
 		remove_action('wp_head', 'feed_links', 10);
 		remove_action('wp_head', 'feed_links_extra', 10);
 		remove_action('wp_head', 'wc_products_rss_feed', 10);
 		remove_action('wp_head', 'wlwmanifest_link');
 		remove_action('wp_head', 'index_rel_link');
 		remove_action('wp_head', 'wp_shortlink_wp_head');
-		remove_action('wp_head', 'rest_output_link_wp_head', 10);
+		remove_action('wp_head', 'wp_oembed_add_host_js');
+		remove_action('wp_head', 'wp_oembed_add_discovery_links');
 		remove_action('wp_head', 'print_emoji_detection_script', 7);
-
+		remove_action('wp_head', 'rest_output_link_wp_head', 10);
 		remove_action('wp_print_styles', 'print_emoji_styles');
 
 		add_filter('the_generator', '__return_false');
