9950adfeb69343c362d68f3aa1a5397c2f0c2692
TwistedAndy <toniyevych@gmail.com>
2016-06-14 15:05:39 +0300

    Fixed error page

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 
-<?php get_template_part('content', 'none'); ?>
+<?php get_template_part('content/none'); ?>
 
 <?php get_footer(); ?>
--- content/none.php
+++ content/none.php
@@ -2,6 +2,6 @@
 
 	<h1><?php echo tw_wp_title(); ?></h1>
 
-	<p><?php tw_none(); ?></p>
+	<p><?php echo tw_none(); ?></p>
 
 </div>
