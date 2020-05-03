7ebe82f98bacc5cc28463c4d4a60c71051b4d976
Toniievych Andrii <toniyevych@gmail.com>
2020-05-03 17:05:05 +0200

    Added functions to work with cookies

---
+++ theme/assets/scripts/cookies.js
@@ -0,0 +1,37 @@
+function setCookie(name, value, exdays) {
+
+	var date = new Date();
+
+	date.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+
+	var expires = 'expires=' + date.toUTCString();
+
+	document.cookie = name + '=' + value + ';' + expires + ';path=/';
+
+}
+
+function getCookie(name) {
+
+	name = name + '=';
+
+	var decodedCookie = decodeURIComponent(document.cookie);
+
+	var parts = decodedCookie.split(';');
+
+	for (var i = 0; i < parts.length; i++) {
+
+		var part = parts[i];
+
+		while (part.charAt(0) === ' ') {
+			part = part.substring(1);
+		}
+
+		if (part.indexOf(name) === 0) {
+			return part.substring(name.length, part.length);
+		}
+
+	}
+
+	return '';
+
+}
