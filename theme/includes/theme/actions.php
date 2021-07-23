b5eed7efe11fc69688598b96cc38923bd247867b
Andrii Toniievych <toniyevych@gmail.com>
2021-07-23 14:01:10 +0200

    Disabled the new widget screen

--- theme/includes/theme/actions.php
+++ theme/includes/theme/actions.php
@@ -158,3 +158,15 @@ add_action('wp_print_styles', function() {
 	}
 
 }, 100);
+
+
+/**
+ * Disables the block editor from managing widgets in the Gutenberg plugin
+ */
+add_filter('gutenberg_use_widgets_block_editor', '__return_false');
+
+
+/**
+ * Disables the block editor from managing widgets
+ */
+add_filter('use_widgets_block_editor', '__return_false');
