c1520b81d2d08db6da844bfd2c407772e05a41bc
Andrii Toniievych <toniyevych@gmail.com>
2024-01-03 11:27:33 +0100

    Moved the loader script to a separate asset

---
+++ theme/assets/plugins/loader/index.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'deps' => ['jquery'],
+	'script' => ['loader.js'],
+	'version' => '4.0'
+);
--- theme/includes/theme/loader.php
+++ theme/includes/theme/loader.php
@@ -361,10 +361,12 @@ function tw_loader_button($wrapper, $template = 'post', $query = false, $number 
 		'query_direction' => $query->get('order')
 	];
 
+	tw_asset_enqueue('loader');
+
 	?>
 
 	<div class="buttons<?php echo $is_hidden ? ' hidden' : ''; ?>">
-		<div class="button outline<?php echo($hidden ? ' is_hidden' : ''); ?>" data-loader="<?php echo htmlspecialchars(json_encode($args), ENT_QUOTES, 'UTF-8'); ?>"><?php _e('Show More', 'twee'); ?></div>
+		<div class="button<?php echo($hidden ? ' is_hidden' : ''); ?>" data-loader="<?php echo htmlspecialchars(json_encode($args), ENT_QUOTES, 'UTF-8'); ?>"><?php _e('Show More', 'twee'); ?></div>
 	</div>
 
 	<?php
