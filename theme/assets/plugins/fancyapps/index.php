d029d1964053982f99fe41ea860513f358b5a1c4
Andrii Toniievych <toniyevych@gmail.com>
2024-01-03 10:46:24 +0100

    Removed the Fancyapps library due to the license restrictions

--- theme/assets/plugins/fancyapps/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'style' => 'fancyapps.css',
-	'script' => 'fancyapps.umd.js',
-	'version' => '5.0.32'
-);
--- theme/assets/plugins/thumbs/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-	'deps' => ['fancyapps'],
-	'script' => 'carousel.thumbs.umd.js',
-	'version' => '5.0.28'
-);
